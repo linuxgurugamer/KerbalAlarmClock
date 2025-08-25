d7e823d848e690f0c029627e2651874d9e10a11a
linuxgurugamer@gmail.com <linuxgurugamer@gmail.com>
2025-08-25 10:14:52 -0400

    Fixed paths to sounds
    Deleted dll

--- KerbalAlarmClock/Utilities.cs
+++ KerbalAlarmClock/Utilities.cs
@@ -28,7 +28,7 @@ namespace KerbalAlarmClock
         internal static String DBPathPlugin = string.Format("TriggerTech/{0}", KerbalAlarmClock._AssemblyName);
         //internal static String DBPathToolbarIcons = string.Format("{0}/ToolbarIcons", DBPathPlugin);
         //internal static String DBPathTextures = string.Format("{0}/Textures", DBPathPlugin);
-        internal static String DBPathPluginSounds = string.Format("{0}/../Sounds", DBPathPlugin);
+        internal static String DBPathPluginSounds = string.Format("{0}/Sounds", DBPathPlugin);
 
         internal static String PathToolbarTexturePath = PathToolbarIcons.Replace("\\", "/").Substring(PathToolbarIcons.Replace("\\", "/").ToLower().IndexOf("/gamedata/") + 10);
 
