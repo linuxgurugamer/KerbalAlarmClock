cc5bddde608c8d874e1be230bd7fc38036756f7b
linuxgurugamer@gmail.com <linuxgurugamer@gmail.com>
2026-05-30 22:02:23 -0400

    update after local build

--- KerbalAlarmClock/AssemblyVersion.cs
+++ KerbalAlarmClock/AssemblyVersion.cs
@@ -5,5 +5,5 @@
   
  using System.Reflection;
 
- [assembly: AssemblyVersion("3.14.2.0")]
- [assembly: AssemblyFileVersion("3.14.2.0")]
+ [assembly: AssemblyVersion("3.14.2.1")]
+ [assembly: AssemblyFileVersion("3.14.2.1")]
